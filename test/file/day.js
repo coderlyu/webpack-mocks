653f79f3aac17aadb7ad4e507749fa9afc0da58e
刘誉 <liuyu02@weidian.com>
2022-02-02 00:12:38 +0800

    refactor: test

---
+++ test/file/day.js
@@ -0,0 +1,1 @@
+console.log('day day up')
--- test/index.js
+++ test/index.js
@@ -3,12 +3,19 @@
 // fs.watch('./t', { recursive: true }, (eventType, filename) => {
 //     console.log(eventType, filename)
 // })
-const t = new Map()
+// const t = new Map()
 
-t.set('s', 'hoishdf')
-t.set('天', '259834yth')
-t.set('b', '和骚ID发')
+// t.set('s', 'hoishdf')
+// t.set('天', '259834yth')
+// t.set('b', '和骚ID发')
 
-t.forEach((...args) => {
-    console.log(args)
-})
+// t.forEach((...args) => {
+//     console.log(args)
+// })
+
+const cwd = process.cwd()
+const path = require('path')
+const t = path.resolve(cwd, './file/day.js')
+console.log(cwd)
+console.log(t)
+console.log(path.relative(__dirname, t))
