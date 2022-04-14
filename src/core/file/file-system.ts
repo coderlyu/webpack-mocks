3b8736d2a600fa92dba7583f34b844ac1a10d6a5
刘誉 <liuyu02@weidian.com>
2022-04-14 18:03:17 +0800

    fix: watch file 问题

--- src/core/file/file-system.ts
+++ src/core/file/file-system.ts
@@ -1,6 +1,7 @@
 import { Options, FileModuleName } from '../../index.d';
 import path from 'path';
-import fs from 'fs';
+// import fs from 'fs';
+import fse from 'fs-extra';
 import mockConfig from '../../config';
 import Module from './file-module';
 import { relativePathFix } from '../../shared/index';
@@ -23,13 +24,13 @@ export default class FileSystem {
   generateRoutes(): void {
     if (this.options.userFolder && this.isDir(this.mockDir)) {
       if (!this.isExistsFile(this.mockDir)) return;
-      let dirs = fs.readdirSync(this.mockDir).map((p) => this.resolve(p, this.mockDir));
+      let dirs = fse.readdirSync(this.mockDir).map((p) => this.resolve(p, this.mockDir));
       while (dirs.length > 0) {
         let _path = dirs.shift();
         if (!_path) continue;
         if (this.isDir(_path)) {
           // 是文件夹
-          dirs = dirs.concat(fs.readdirSync(_path).map((p) => this.resolve(p, _path)));
+          dirs = dirs.concat(fse.readdirSync(_path).map((p) => this.resolve(p, _path)));
         } else {
           // 是文件
           this.readFileByOrder(_path); // 读取文件并缓存
@@ -47,36 +48,46 @@ export default class FileSystem {
   require(filePath: string, type: string, refresh?: boolean): FileModuleName {
     if (this.modules.has(filePath) && !refresh) return this.modules.get(filePath);
     // json, js, ts 文件
+    let file = '';
     try {
       delete require.cache[filePath + '.' + type]; // 清除 require 缓存
-      const file = require(filePath + '.' + type);
-      if (refresh) this.modules.set(filePath, file);
-      else this.modules.add(filePath, file, this.relativePath(filePath), type);
+      file = require(filePath + '.' + type);
     } catch (error) {
       logger.warn(`${filePath} is not a valid file`);
       logger.error(JSON.stringify(error));
-      return undefined;
+      file = '';
     }
+    if (refresh) {
+      if (this.modules.has(filePath)) {
+        this.modules.add(filePath, file, this.relativePath(filePath), type);
+        bus.emit('generateRoutes'); // 生成路由
+      } else this.modules.set(filePath, file);
+    } else this.modules.add(filePath, file, this.relativePath(filePath), type);
     return this.modules.get(filePath);
   }
   isDir(filePath: string): boolean {
     if (!this.isExistsFile(filePath)) return false;
-    return fs.lstatSync(filePath).isDirectory(); // 判断是否是目录
+    return fse.lstatSync(filePath).isDirectory(); // 判断是否是目录
   }
   isExistsFile(filePath: string): boolean {
-    return fs.existsSync(filePath);
+    return fse.existsSync(filePath);
   }
   readFile(filePath: string, type: string, refresh?: boolean): FileModuleName {
     // 其他文件类型
     if (this.modules.has(filePath) && !refresh) return this.modules.get(filePath);
+    let file = '';
     try {
-      const file = fs.readFileSync(filePath + '.' + type).toString();
-      if (refresh) this.modules.set(filePath, file);
-      else this.modules.add(filePath, file, this.relativePath(filePath), type);
+      file = fse.readFileSync(filePath + '.' + type).toString();
     } catch (error) {
       logger.warn(`${filePath} is not a valid file`);
-      return undefined;
+      file = '';
     }
+    if (refresh) {
+      if (this.modules.has(filePath)) {
+        this.modules.add(filePath, file, this.relativePath(filePath), type);
+        bus.emit('generateRoutes'); // 生成路由
+      } else this.modules.set(filePath, file);
+    } else this.modules.add(filePath, file, this.relativePath(filePath), type);
     return this.modules.get(filePath);
   }
   /**
@@ -125,6 +136,10 @@ export default class FileSystem {
     this.modules.clear();
     this.generateRoutes();
   }
+  fineUnlink(filename: string) {
+    this.modules.clear();
+    this.generateRoutes();
+  }
   relativePath(_path: string) {
     return relativePathFix(path.relative(this.mockDir, _path));
   }
@@ -140,6 +155,13 @@ export default class FileSystem {
           break;
         case 'unlink':
           // not support
+          // 删除 index.ts 同级的 json 文件，因为该 json 文件是根据 index.ts 生成的
+          // 此时应该判断是否存在 index.ts，如果存在，应该继续 生成 ts 的 mock 数据
+          const type = path.slice(path.lastIndexOf('.') + 1);
+          if (type === 'json') {
+            const tsFile = `${path.slice(0, path.lastIndexOf('.'))}.ts`;
+            if (fse.existsSync(tsFile)) this.fineUnlink(path);
+          }
           break;
         default:
           break;
